03fe484ba6929be5dd626a32ad146543944fb5a2
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-10-23 08:36:11 +0500

    hide comment and buttons when signout

--- components/Feed/FeedComponents/Posts/Post.jsx
+++ components/Feed/FeedComponents/Posts/Post.jsx
@@ -1,9 +1,10 @@
 import { BookmarkAltIcon, ChatIcon, DotsCircleHorizontalIcon, EmojiHappyIcon, HeartIcon, PaperAirplaneIcon } from "@heroicons/react/outline"
 
 import { HeartIcon as HearIconFilled } from "@heroicons/react/solid"
-
+import { useSession } from 'next-auth/react'
 
 function Post({id, username, userImg, img, caption}) {
+    const { data: session } = useSession()
     return (
         <div className="bg-white my-7 border rounded-sm">
 
@@ -22,19 +23,23 @@ function Post({id, username, userImg, img, caption}) {
 
             {/*img*/}
             <img className="object-cover w-full" src={img} alt=""/>
-
-            {/*Buttons*/}
-            <div className="flex justify-between px-4 pt-4">
-
-                <div className="flex items-center space-x-4">
-                    <HeartIcon className="btn"/>
-                    <ChatIcon className="btn"/>
-                    <PaperAirplaneIcon className="btn"/>
-                </div>
-
-                <BookmarkAltIcon className="btn"/>
             
-            </div>
+            {/*Buttons*/}
+            {session && (
+                
+                <div className="flex justify-between px-4 pt-4">
+
+                    <div className="flex items-center space-x-4">
+                        <HeartIcon className="btn"/>
+                        <ChatIcon className="btn"/>
+                        <PaperAirplaneIcon className="btn"/>
+                    </div>
+
+                    <BookmarkAltIcon className="btn"/>
+                
+                </div>
+        
+            )}
             
 
             {/*caption*/}
@@ -45,13 +50,16 @@ function Post({id, username, userImg, img, caption}) {
 
 
             {/*input box*/}
-            <form className="flex items-center p-4">
-                <EmojiHappyIcon className="h-7"/>
-                <input className="flex-1 border-none focus:ring-0 outline-none"
-                type="text"
-                placeholder="Add a Comment..."/>
-                <button className="font-semibold text-blue-400">Post</button>
-            </form>
+            {session && (
+                <form className="flex items-center p-4">
+                    <EmojiHappyIcon className="h-7"/>
+                    <input className="flex-1 border-none focus:ring-0 outline-none"
+                    type="text"
+                    placeholder="Add a Comment..."/>
+                    <button className="font-semibold text-blue-400">Post</button>
+                </form>
+            )}
+            
         </div>
     )
 }
